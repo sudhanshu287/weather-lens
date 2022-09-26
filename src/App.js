5aa885d522a5fa98b6d0336d06703cf2e1dfb5ba
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2022-09-26 22:47:43 +0530

    Project Bug Fix

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const App = () => {
             }
           />
 
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
