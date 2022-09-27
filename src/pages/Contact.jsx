984de9ea6606186746b7c9bf05c158a20d0cf965
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2022-09-27 16:51:55 +0530

    Project Completed 2.0

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,51 +3,66 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 const Contact = () => {
   return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-12">
-            <h1 className="dispaly-2 fw-bold text-center">Contact Us</h1>
+    <>
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-12 my-5">
+              <h1 className="dispaly-2 fw-bold text-center">Contact Us</h1>
+            </div>
           </div>
         </div>
-        <form className="mt-5" style={{display:"flex",justifyContent:"center"}}>
-          <div style={{width:"50%"}} className="form-box border rounded py-5 px-5">
-            <div class="form-group my-2">
-              <label for="inp1">Your Name*</label>
-              <input
-                type="text"
-                class="form-control"
-                id="inp1"
-                placeholder="Enter Your Name"
-                required
-              />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <form className="border border-primary rounded py-5 px-5">
+            <div className="row">
+              <div className="col-sm-12 my-2">
+                <div class="form-group">
+                  <label for="nameInp">Your Name*</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="nameInp"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter Your Name"
+                    required
+                  />
+                </div>
+              </div>
             </div>
-            <div class="form-group my-4">
-              <label for="inp2">Email address*</label>
-              <input
-                type="email"
-                class="form-control"
-                id="inp2"
-                placeholder="Enter Your Email"
-                required
-              />
+            <div className="row">
+              <div className="col-sm-12 my-2">
+                <div class="form-group">
+                  <label for="emailInp">Enter Your Email*</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="emailInp"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter Your Email"
+                    required
+                  />
+                </div>
+              </div>
             </div>
-            <div class="form-group">
-              <label for="text-area">Message*</label>
-              <textarea
-                class="form-control"
-                id="text-area"
-                rows="3"
-                required
-                placeholder="Your Message. . . . . ."
-              ></textarea>
+            <div className="row">
+              <div className="col-sm-12 my-2">
+                <div class="form-group">
+                  <label for="text-area">Message*</label>
+                  <textarea
+                    class="form-control"
+                    id="text-area"
+                    rows="3"
+                    required
+                  ></textarea>
+                </div>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
