e2db78f97560eb09431215aee92995fb0c135a37
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2022-10-07 15:24:25 +0530

    Final code done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React from 'react'
-import { Route, BrowserRouter, Routes, } from "react-router-dom";
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About
- from './pages/About';
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route
-            index
-            element={
-              
-                <Home />
-              
-            }
-          />
+          <Route index element={<Home />} />
 
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
