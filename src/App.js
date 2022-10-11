dda3315095a890910294c3d767d8e77918f84216
Sudhanshu Patel <patelsudhanshu290@gmail.com>
2022-10-12 00:52:26 +0530

    scroll bar effect added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import {Helmet} from 'react-helmet';
 const App = () => {
   return (
     <BrowserRouter>
+    <Helmet>
+      <title>Weather Lens</title>
+    </Helmet>
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
